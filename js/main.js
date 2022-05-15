563d823f1aa119ddd2b4a11e847e976b8895e0b1
owhuo.nkem <owhuonkem2006@gmail.com>
2022-05-15 12:18:29 +0100

    setting up my project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var   x, y;
+x = document.getElementById("passcode")
+y = document.getElementById("show")
+
+ function passwordToggle (){
+    if(x.type === "password" && x.value !=""){
+        x.type = "text"; 
+        y.classList.remove("bx-low-vision"); 
+        y.classList.add("bx-bullseye");
+    }else
+    {
+        x.type = "password";
+        y.classList.remove("bx-bullseye");
+        y.classList.add("bx-low-vision"); 
+
+    }
+}
+
