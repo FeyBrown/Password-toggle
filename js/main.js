6b1ea992b8add41504b116448a14cdb08ba505b2
owhuo.nkem <owhuonkem2006@gmail.com>
2022-05-19 09:28:16 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@ var   x, y;
 x = document.getElementById("passcode")
 y = document.getElementById("show")
  function passwordToggle (){
-    if(x.type === "password" && x.value !=""){
+    if(x.type === "password"){
         x.type = "text"; 
-        y.classList.remove("bx-low-vision"); 
-        y.classList.add("bx-bullseye");
+        y.classList.remove("bx-hide"); 
+        y.classList.add("bx-show");
     }else
     {
         x.type = "password";
-        y.classList.remove("bx-bullseye");
-        y.classList.add("bx-low-vision"); 
+        y.classList.remove("bx-show");
+        y.classList.add("bx-hide"); 
 
     }
 }
